d452516b7aa3645c05605a244222b4501e24e2a9
Yngve Bakken-Nilsen <yngve.nilsen@gmail.com>
2015-02-13 09:36:33 +0100

    fix

--- do-stuff.ps1
+++ do-stuff.ps1
@@ -8,16 +8,16 @@
 & git checkout -b HOT_FIX
 & git push --set-upstream origin HOT_FIX
 & git checkout MY_FEAT
-echo Hello > lkdsjflk.txt
+echo Hello >> lkdsjflk.txt
 
-echo world > lsadkjflsaj.bla
+echo world >> lsadkjflsaj.bla
 & git add . -A
 & git commit -m'some stuff'
 & git push
 
 & git checkout master
-echo You > test2.txt
-echo fix > test2.txt
+echo You >> test2.txt
+echo fix >> test2.txt
 & git add . -A
 & git commit -m'some stuff'
 & git push
@@ -25,11 +25,11 @@ echo fix > test2.txt
 
 & git checkout HOT_FIX
 
-echo fix > test3.txt
+echo fix >> test3.txt
 & git add . -A
 & git commit -m'some stuff'
 & git push
-echo fix2 > test3.txt
+echo fix2 >> test3.txt
 & git add . -A
 & git commit -m'some stuff'
 & git push
