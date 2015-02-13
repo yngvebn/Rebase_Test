ba86597131e4e67caac779d44ec0a0e6f68751b2
Yngve Bakken-Nilsen <yngve.nilsen@gmail.com>
2015-02-13 09:30:41 +0100

    dostuff.bat

--- do-stuff.bat
+++ do-stuff.bat
@@ -1,3 +1,5 @@
+git branch -D MY_FEAT
+git branch -D HOT_FIX
 git push origin --delete HOT_FIX
 git push origin --delete MY_FEAT
 
@@ -10,14 +12,14 @@ echo Hello > lkdsjflk.txt
 
 echo world > lsadkjflsaj.bla
 git add . -A
-git commit -m'some stuff' -a
+git commit -m'some stuff'
 git push
 
 git checkout master
 echo You > test2.txt
 echo fix > test2.txt
 git add . -A
-git commit -m'some stuff' -a
+git commit -m'some stuff'
 git push
 git checkout MY_FEAT
 
@@ -25,9 +27,9 @@ git checkout HOT_FIX
 
 echo fix > test3.txt
 git add . -A
-git commit -m'some stuff' -a
+git commit -m'some stuff'
 git push
 echo fix2 > test3.txt
 git add . -A
-git commit -m'some stuff' -a
+git commit -m'some stuff'
 git push
